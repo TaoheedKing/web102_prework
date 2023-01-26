c33d182d46d8a45cbcf24c779c7669d4370b6596
Taoheed King <taoheedking@Taoheeds-MacBook-Pro.local>
2023-01-26 14:45:24 -0500

    Working on Challenege 3 failing to show up on web

--- index.js
+++ index.js
@@ -29,25 +29,30 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
+    for (let i = 0; i < games.length; i++) {
+        const game = games[i];
 
         // create a new div element, which will become the game card
-
-
+        const div = document.createElement("div");
         // add the class game-card to the list
-
+        div.classList.add("game-card");
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
+        div.innerHTML = `<h1>Name: ${game.name}</h1>
+        <p>${game.description}</p>
+        <img src=${game.img} />`;
 
         // append the game to the games-container
-
+        let gamesContainer = document.querySelector('games-container');
+        gamesContainer.append(div);
+    }
 }
 
 // call the function we just defined using the correct variable
+addGamesToPage(GAMES_JSON)
 // later, we'll call this function using a different list of games
 
 
